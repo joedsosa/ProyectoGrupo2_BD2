f56d07573168f1b5312d5fdfb735db93880c70de
joedsosa <115840212+joedsosa@users.noreply.github.com>
2024-05-12 23:52:31 -0600

    Add files via upload

---
+++ src/proyecto/db2/ProyectoDB2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.db2;
+
+/**
+ *
+ * @author HTS
+ */
+public class ProyectoDB2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LogIn l = new LogIn();
+        l.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
